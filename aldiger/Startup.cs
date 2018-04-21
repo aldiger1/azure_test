7210bbfb0d091d9f05c621541ae548e07f5f1f36
Aldiger <aldiger.m@scopicsoftware.com>
2018-04-22 01:38:28 +0200

    First commit

---
+++ aldiger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aldiger.Startup))]
+namespace aldiger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
